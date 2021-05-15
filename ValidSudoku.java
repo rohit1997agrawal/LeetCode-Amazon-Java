d4193523a90c45456fccfc2e00af3083262a81df
rohit1997agrawal <rohit1997agrawal@gmail.com>
2021-05-16 01:37:53 +0530

    Update ValidSudoku.java

--- ValidSudoku.java
+++ ValidSudoku.java
@@ -4,7 +4,7 @@ class Solution {
         if(board == null)
             return false;
         
-       HashSet<String> set = new HashSet<String>();       
+       Set<String> set = new HashSet<String>();       
         for(int i = 0 ; i< 9; i++)
         {
             for(int j= 0 ; j<9 ; j++)
