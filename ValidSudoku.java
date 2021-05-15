7428cf4ed6c997ff603255045b0fce6dcff1d5d9
rohit1997agrawal <rohit1997agrawal@gmail.com>
2021-05-16 01:37:25 +0530

    Create ValidSudoku.java

---
+++ ValidSudoku.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        
+        if(board == null)
+            return false;
+        
+       HashSet<String> set = new HashSet<String>();       
+        for(int i = 0 ; i< 9; i++)
+        {
+            for(int j= 0 ; j<9 ; j++)
+            {
+                int num= board[i][j]-'0';
+                if(num>=1 && num<=9)
+                {
+                    int block = (i / 3 * 3) + (j / 3);
+                    if(!set.add("row"+i+num)||
+                       !set.add("col"+j+num)||
+                       !set.add("block"+block+num)
+                    )
+                    {
+                        return false;
+                    }
+                   
+               }
+            }
+        }
+        return true;
+        
+    }
+}
